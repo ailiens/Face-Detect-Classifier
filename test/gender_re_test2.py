84527565ec34d2b2b6248db5baf84e1635b7942f
HJ <stotmyst@gmail.com>
2023-10-11 19:41:52 +0900

    notebook change

--- test/gender_re_test2.py
+++ test/gender_re_test2.py
@@ -28,28 +28,28 @@ def detect_face(image_path):
             cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
         face_img = cv2.resize(img[y1:y2, x1:x2], (224, 224))
     return face_img
-
-#### 이미지 불러오기
-# test_image_path = '../test/image/image.jpg'   ### no  >> OK
-# test_image_path = '../test/image/ma.jpg'    # OK
-# test_image_path = '../test/image/sun.jpg'    ### no
-# test_image_path = '../test/image/nana.jpg'    # OK
-# test_image_path = '../test/image/nana2.jpg'   # OK
-# test_image_path = '../test/image/goong.jpg'    # OK
-# test_image_path = '../test/image/jungi.jpg'   ### no
-# test_image_path = '../test/image/jo.jpg'   # no 조혜련 태보
-# test_image_path = '../test/image/jo2.jpg'   # 조혜련 OK
-# test_image_path = '../test/image/yeop.jpg'   # 여장 신동엽 OK
+#
+# #### 이미지 불러오기
+# # test_image_path = '../test/image/image.jpg'   ### no  >> OK
+# # test_image_path = '../test/image/ma.jpg'    # OK
+# # test_image_path = '../test/image/sun.jpg'    ### no
+# # test_image_path = '../test/image/nana.jpg'    # OK
+# # test_image_path = '../test/image/nana2.jpg'   # OK
+# # test_image_path = '../test/image/goong.jpg'    # OK
+# # test_image_path = '../test/image/jungi.jpg'   ### no
+# # test_image_path = '../test/image/jo.jpg'   # no 조혜련 태보
+# # test_image_path = '../test/image/jo2.jpg'   # 조혜련 OK
+# # test_image_path = '../test/image/yeop.jpg'   # 여장 신동엽 OK
 test_image_path = '../test/image/cheol.jpg'   # 김희철 OK
 
 face_image = detect_face(test_image_path)
 face_image_pil = Image.fromarray(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
 
-# 이미지 표시
-plt.imshow(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
-plt.axis('off')
-plt.title('Cropped Face')
-plt.show()
+# # 이미지 표시
+# plt.imshow(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
+# plt.axis('off')
+# plt.title('Cropped Face')
+# plt.show()
 
 # 이미지 전처리를 위한 transform
 transform = transforms.Compose([
@@ -71,16 +71,21 @@ model.fc = nn.Linear(model.fc.in_features, 2)
 model = model.to(device)
 
 model.load_state_dict(torch.load('../models/1010_gender_classification_model_re_under.pth', map_location=device))
-model.eval()
 
-with torch.no_grad():
-    input_batch = input_batch.to(device)
-    output = model(input_batch)
-    _, predicted = torch.max(output, 1)
-    print(f'Predicted age range: {predicted.item()}')  # 예측된 성별 출력
-    class_labels = ['Male', 'Female']
-    predicted_class = class_labels[predicted.item()]
-    print(f'Predicted gender: {predicted_class}')
+from torchsummary import summary
+summary(model, (3, 224, 224))
+
+
+# model.eval()
+#
+# with torch.no_grad():
+#     input_batch = input_batch.to(device)
+#     output = model(input_batch)
+#     _, predicted = torch.max(output, 1)
+#     print(f'Predicted age range: {predicted.item()}')  # 예측된 성별 출력
+#     class_labels = ['Male', 'Female']
+#     predicted_class = class_labels[predicted.item()]
+#     print(f'Predicted gender: {predicted_class}')
 
 
 
